b8b78281447907e0a097ff1c0616346b28ec2e44
114042-TOLEDO-BRUNO <114042@tecnicatura.frc.utn.edu.ar>
2023-08-30 15:21:48 -0300

    resttemplate builder

--- src/main/java/ar/edu/utn/frc/tup/lciv/resttemplatepractice/Config/RestClientConfiguration.java
+++ src/main/java/ar/edu/utn/frc/tup/lciv/resttemplatepractice/Config/RestClientConfiguration.java
@@ -1,13 +1,23 @@
 package ar.edu.utn.frc.tup.lciv.resttemplatepractice.Config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
+
+
 @Configuration
 public class RestClientConfiguration {
+    private static final int TIME_OUT=1000;//1 segundo
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        //return new RestTemplate();
+        return builder//patron de diseño builder.
+                .setConnectTimeout(Duration.ofMillis(TIME_OUT))//de conexion
+                .setReadTimeout(Duration.ofMillis(TIME_OUT))//de lectura
+                .build();
     }
 }
